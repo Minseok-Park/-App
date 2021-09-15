d7465437917d42d212e22810eb79f078ac4b5a2f
minseok-Park <pms0905@naver.com>
2021-09-15 18:01:46 +0900

    card 레이아웃 구성

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./maker.module.css";
 import Header from "../header/header";
 import Footer from "../footer/footer";
@@ -7,6 +7,41 @@ import Preview from "../preview/preview";
 import { useHistory } from "react-router";
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: "1",
+      name: "Ellie",
+      company: "Samsung",
+      theme: "light",
+      title: "Software Engineer",
+      email: "ellie@gmail.com",
+      message: "go for it",
+      fileName: "ellie",
+      fileURL: null,
+    },
+    {
+      id: "2",
+      name: "Andrew",
+      company: "Coupang",
+      theme: "dark",
+      title: "Software Engineer",
+      email: "ellie@gmail.com",
+      message: "go for it",
+      fileName: "ellie",
+      fileURL: null,
+    },
+    {
+      id: "3",
+      name: "Mike",
+      company: "LG",
+      theme: "colorful",
+      title: "Software Engineer",
+      email: "ellie@gmail.com",
+      message: "go for it",
+      fileName: "ellie",
+      fileURL: null,
+    },
+  ]);
   const history = useHistory();
 
   const onLogout = () => {
@@ -25,8 +60,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards} />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
